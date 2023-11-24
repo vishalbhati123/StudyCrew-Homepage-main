70431ea47c02b713ba8f329c72f120ec2ade412e
vishalbhati123 <vishalgbhati@gmail.com>
2023-11-24 17:35:29 +1300

    Responsive Fixed-Faq, hero, Project,Empower, Team

---
+++ client/src/data/teamData.js
@@ -0,0 +1,43 @@
+// teamData.js
+
+import jacob from "../assets/avatars/jacob.svg";
+import jona from "../assets/avatars/jona.svg";
+import sugumaran from "../assets/avatars/sugumaran.svg";
+import xavier from "../assets/avatars/xavier.svg";
+import nicholas from "../assets/avatars/nicholas.svg";
+import kian from "../assets/avatars/kian.svg";
+
+const teamMembers = [
+  {
+    name: "Jacob",
+    role: "Founder",
+    avatar: jacob
+  },
+  {
+    name: "Jona",
+    role: "Marketing",
+    avatar: jona
+  },
+  {
+    name: "Sugumaran",
+    role: "Development",
+    avatar: sugumaran
+  },
+  {
+    name: "Xavier",
+    role: "Product Management",
+    avatar: xavier
+  },
+  {
+    name: "Nicholas",
+    role: "Marketing",
+    avatar: nicholas
+  },
+  {
+    name: "Kian",
+    role: "UI/UX Design",
+    avatar: kian
+  },
+];
+
+export default teamMembers;
